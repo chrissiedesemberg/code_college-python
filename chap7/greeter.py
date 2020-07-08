90013f9d8110bd87eafb4eda4e24ebfd3ce24c2e
“Chrissie <chrizelda.desemberg@gmail.com>
2020-07-08 13:24:22 +0200

    Chapter 6 completed

---
+++ chap7/greeter.py
@@ -0,0 +1,13 @@
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
+
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print(f"\nHello, {name}!")
+
+age = input("How old are you? ")
+age = int(age)
+print(age >= 18)
+
