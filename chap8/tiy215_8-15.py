d3337bad935cf40978d73081371df19f38e1f675
“Chrissie <chrizelda.desemberg@gmail.com>
2020-07-08 15:10:21 +0200

    Chapter 8-15 updated

--- chap8/tiy215_8-15.py
+++ chap8/tiy215_8-15.py
@@ -1,13 +1,20 @@
-from chap8.messages import send_messages
+from printing_functions import print_models, show_completed_models
 
-short_messages = [
-    "Hi there, champ!",
-    "Whatcha doing good-looking?",
-    "Catch you later"
-]
-sent_messages = []
+# Start with some designs that need to be printed.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
 
-send_messages()
+# Simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design)
 
-print(sent_messages)
-print(short_messages)
+# Display all completed models.
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
